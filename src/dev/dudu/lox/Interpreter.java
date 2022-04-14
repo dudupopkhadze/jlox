86658d9157652889c69ea4dca450199607f3e22e
Dudu Popkhadze <dudupopkhadze@gmail.com>
2022-04-14 19:32:57 +0400

    simple env

--- src/dev/dudu/lox/Interpreter.java
+++ src/dev/dudu/lox/Interpreter.java
@@ -3,6 +3,7 @@ package dev.dudu.lox;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>,  Stmt.Visitor<Void> {
+    private Environment environment = new Environment();
 
     void interpret(List<Stmt> statements) {
         try {
@@ -45,14 +46,18 @@ class Interpreter implements Expr.Visitor<Object>,  Stmt.Visitor<Void> {
 
     @Override
     public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+        environment.define(stmt.name.lexeme, value);
         return null;
     }
 
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        return null;
+        return environment.get(expr.name);
     }
-
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
